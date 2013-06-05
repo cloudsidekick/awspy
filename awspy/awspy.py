031f5de100852bbeef0e141fd8d3c195593b548d
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2013-06-05 15:54:32 -0400

    added route53 support

--- awspy/awspy.py
+++ awspy/awspy.py
@@ -38,7 +38,7 @@ versions = {
     "vpc" : "2010-11-15",
     "iam" : "2010-05-08",
     "sdb" : "2009-04-15",
-    "r53" : "2010-10-01",
+    "r53" : "2012-12-12",
     "ebs" : "2010-12-01"}
 
 # the newer products need to be added here
@@ -57,7 +57,7 @@ end_names = {
     "vpc" : "ec2",
     "iam" : "iam",
     "sdb" : "sdb",
-    "r53" : "r53",
+    "r53" : "route53",
     "ebs" : "elasticbeanstalk"}
 
 
@@ -83,6 +83,7 @@ class AWSConn():
         self.path = path 
         self.timeout = timeout
         self.debug = debug
+        self.product = product
 
         if not api_version:
             self.api_version = versions[product]
@@ -107,23 +108,48 @@ class AWSConn():
         end_name = end_names[product]
 
         # some aws products don't conform to the following, needs fixing up
-        if not region or (product == "sdb" and region == "us-east-1") or product == "iam":
+        if not region or (product == "sdb" and region == "us-east-1") or product in ["iam", "r53"]:
             endpoint = "%s.amazonaws.com" % (end_name)
         else:
             endpoint = "%s.%s.amazonaws.com" % (end_name, region)
         return endpoint
 
         
-    def aws_query(self, action, action_params=[]):
-        """aws_query - queries the AWS compatible endpoint and returns the pure XML response
-            throws an exception on error
+    def _build_r53_request(self, action, action_params, request_type, data):
 
-            action - the AWS action, e.g. "DescribeInstances"
-            action_params - a list of key, value pairs that correpond to the parameters
-                of the action"""
+        baseurl = "%s://%s" % (self.protocol.lower(), self.endpoint)
 
-        # to do - some of the older and newer AWS products use different signing methods
-        # this needs to be reflected in the code below
+        url = "%s/%s" % (baseurl, "date")
+        response = urllib2.urlopen(url, None, self.timeout)
+        timestamp = response.info().getheader("Date")
+        print timestamp
+
+        url = "%s/%s/%s" % (baseurl, self.api_version, action)
+        print url
+
+        digest = hmac.new(self.secret_key, timestamp, hashlib.sha256).digest()
+        signature = base64.b64encode(digest)
+        xauth = "AWS3-HTTPS AWSAccessKeyId=%s,Algorithm=HmacSHA256,Signature=%s" % (self.access_key, signature)
+
+        req = urllib2.Request(url)
+        req.get_method = lambda: request_type
+        if data and not len(data):
+            data = None
+        elif data and len(data):
+            length = str(len(data))
+            req.add_header("Content-Length", length)
+            
+        req.add_data(data)
+
+        req.add_header("X-Amzn-Authorization", xauth)
+        req.add_header("x-amz-date", timestamp)
+        req.add_header("Content-Type", "text/plain")
+        req.add_header("Host", "route53.amazonaws.com")
+
+        return req
+
+    
+    def _build_querystring_request(self, action, action_params):
 
         params = {}
         params["Timestamp"] = time.strftime("%Y-%m-%dT%H:%M:%S.000Z", time.gmtime())
@@ -132,10 +158,9 @@ class AWSConn():
         params["SignatureMethod"] = "HmacSHA256"
         params["SignatureVersion"] = "2"
         params["AWSAccessKeyId"] = self.access_key
-
         params_list = params.items() + action_params
-        
         params_list.sort()
+
         if self.debug:
             print params_list
         query_string = '&'.join(['%s=%s' % (k,urllib2.quote(str(v))) for (k,v) in params_list if v])
@@ -144,15 +169,45 @@ class AWSConn():
         signature = urllib2.quote(base64.b64encode(digest))
 
         url = "%s://%s%s?%s&Signature=%s" % (self.protocol.lower(), self.endpoint, self.path, query_string, signature)
+
         if self.debug:
             print url
 
+        req = urllib2.Request(url)
+
+        return req
+
+    def aws_query(self, action, action_params=[], request_type="GET", data=None):
+        """aws_query - queries the AWS compatible endpoint and returns the pure XML response
+            throws an exception on error
+
+            action - the AWS action, e.g. "DescribeInstances"
+            action_params - a list of key, value pairs that correpond to the parameters
+                of the action
+            request_type - either GET, POST, DELETE, GET default
+            data - POST or DELETE data"""
+
+        # to do - some of the older and newer AWS products use different signing methods
+        # this needs to be reflected in the code below
+
+        if request_type not in ["GET", "POST", "DELETE"]:
+            raise Exception("aws_query request type of %s unsupported. Must be one of GET, POST, DELETE" % (request_type))
+
+
+        if self.product != "r53":
+            req = self._build_querystring_request(action, action_params)
+        else:
+            req = self._build_r53_request(action, action_params, request_type, data)
+
+
+
         try:
-            response = urllib2.urlopen(url, None, self.timeout)
+            #response = urllib2.urlopen(url, None, self.timeout)
+            response = urllib2.urlopen(req, None,  self.timeout)
         except urllib2.HTTPError, e:
-            raise Exception("HTTPError = %s, %s, %s\n%s" % (str(e.code), e.msg, e.read(), url))
+            raise Exception("HTTPError = %s, %s, %s" % (str(e.code), e.msg, e.read()))
         except urllib2.URLError, e:
-            raise Exception("URLError = %s\n%s" % (str(e.reason), url))
+            raise Exception("URLError = %s" % (str(e.reason)))
         except httplib.HTTPException, e:
             raise Exception("HTTPException")
         except Exception:
